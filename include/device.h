14829869447fdb0be99763c60b88fd6a75424693
Julian Waller <git@julusian.co.uk>
2019-05-04 13:27:52 +0100

    feat: Further implement output device

--- include/device.h
+++ include/device.h
@@ -58,7 +58,42 @@ cdecklink_device_output_create_video_frame(cdecklink_device_output_t *output, in
 
 //virtual HRESULT CreateAncillaryData (/* in */ BMDPixelFormat pixelFormat, /* out */ IDeckLinkVideoFrameAncillary **outBuffer) = 0;
 
-HRESULT cdecklink_device_output_display_video_frame_sync (cdecklink_device_output_t *output, cdecklink_video_frame_t *frame);
+HRESULT
+cdecklink_device_output_display_video_frame_sync(cdecklink_device_output_t *output, cdecklink_video_frame_t *frame);
+
+HRESULT
+cdecklink_device_output_schedule_video_frame(cdecklink_device_output_t *output, cdecklink_video_frame_t *theFrame,
+                                             int64_t displayTime, int64_t displayDuration, int64_t timeScale);
+
+//HRESULT cdecklink_device_output_set_scheduled_frame_completion_callback (cdecklink_device_output_t *output, /* in */ IDeckLinkVideoOutputCallback *theCallback) = 0;
+HRESULT
+cdecklink_device_output_buffered_video_frame_count(cdecklink_device_output_t *output, uint32_t *bufferedFrameCount);
+
+/* Audio Output */
+
+HRESULT cdecklink_device_output_enable_audio_output(cdecklink_device_output_t *output, BMDAudioSampleRate sampleRate,
+                                                    BMDAudioSampleType sampleType, uint32_t channelCount,
+                                                    BMDAudioOutputStreamType streamType);
+
+HRESULT cdecklink_device_output_disable_audio_output(cdecklink_device_output_t *output);
+
+HRESULT cdecklink_device_output_write_audio_samples_sync(cdecklink_device_output_t *output, void *buffer,
+                                                         uint32_t sampleFrameCount, uint32_t *sampleFramesWritten);
+
+HRESULT cdecklink_device_output_begin_audio_preroll(cdecklink_device_output_t *output);
+
+HRESULT cdecklink_device_output_end_audio_preroll(cdecklink_device_output_t *output);
+
+HRESULT cdecklink_device_output_schedule_audio_samples(cdecklink_device_output_t *output, void *buffer,
+                                                       uint32_t sampleFrameCount, int64_t streamTime,
+                                                       int64_t timeScale, uint32_t *sampleFramesWritten);
+
+HRESULT cdecklink_device_output_buffered_audio_sample_frame_count(cdecklink_device_output_t *output,
+                                                                  uint32_t *bufferedSampleFrameCount);
+
+HRESULT cdecklink_device_output_flush_buffered_audio_samples(cdecklink_device_output_t *output);
+
+//virtual HRESULT SetAudioCallback (/* in */ IDeckLinkAudioOutputCallback *theCallback) = 0;
 
 #ifdef __cplusplus
 };
--- src/device.cpp
+++ src/device.cpp
@@ -154,3 +154,97 @@ cdecklink_device_output_display_video_frame_sync(cdecklink_device_output_t *outp
 
     return S_FALSE;
 }
+
+HRESULT
+cdecklink_device_output_schedule_video_frame(cdecklink_device_output_t *output, cdecklink_video_frame_t *theFrame,
+                                             int64_t displayTime, int64_t displayDuration, int64_t timeScale) {
+    if (output != nullptr && output->obj != nullptr) {
+        IDeckLinkVideoFrame *frame = nullptr;
+        if (theFrame != nullptr) frame = theFrame->obj;
+
+        return output->obj->ScheduleVideoFrame(frame, displayTime, displayDuration, timeScale);
+    }
+
+    return S_FALSE;
+}
+
+HRESULT
+cdecklink_device_output_buffered_video_frame_count(cdecklink_device_output_t *output, uint32_t *bufferedFrameCount) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->GetBufferedVideoFrameCount(bufferedFrameCount);
+    }
+
+    return S_FALSE;
+}
+
+/* Audio Output */
+
+HRESULT cdecklink_device_output_enable_audio_output(cdecklink_device_output_t *output, BMDAudioSampleRate sampleRate,
+                                                    BMDAudioSampleType sampleType, uint32_t channelCount,
+                                                    BMDAudioOutputStreamType streamType) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->EnableAudioOutput(sampleRate, sampleType, channelCount, streamType);
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_disable_audio_output(cdecklink_device_output_t *output) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->DisableAudioOutput();
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_write_audio_samples_sync(cdecklink_device_output_t *output, void *buffer,
+                                                         uint32_t sampleFrameCount, uint32_t *sampleFramesWritten){
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->WriteAudioSamplesSync(buffer, sampleFrameCount, sampleFramesWritten);
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_begin_audio_preroll(cdecklink_device_output_t *output){
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->BeginAudioPreroll();
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_end_audio_preroll(cdecklink_device_output_t *output) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->EndAudioPreroll();
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_schedule_audio_samples(cdecklink_device_output_t *output, void *buffer,
+                                                       uint32_t sampleFrameCount, int64_t streamTime,
+                                                       int64_t timeScale, uint32_t *sampleFramesWritten) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->ScheduleAudioSamples(buffer, sampleFrameCount, streamTime, timeScale, sampleFramesWritten);
+    }
+
+    return S_FALSE;
+}
+
+HRESULT cdecklink_device_output_buffered_audio_sample_frame_count(cdecklink_device_output_t *output, uint32_t *bufferedSampleFrameCount) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->GetBufferedAudioSampleFrameCount(bufferedSampleFrameCount);
+    }
+
+    return S_FALSE;
+}
+HRESULT cdecklink_device_output_flush_buffered_audio_samples(cdecklink_device_output_t *output) {
+    if (output != nullptr && output->obj != nullptr) {
+        return output->obj->FlushBufferedAudioSamples();
+    }
+
+    return S_FALSE;
+}
+
+//virtual HRESULT SetAudioCallback (/* in */ IDeckLinkAudioOutputCallback *theCallback) = 0;
